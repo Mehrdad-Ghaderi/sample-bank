4d0c0cc67d6b3c9c119ce4a1bf06060f3d75dcfc
MehRdad <mehrdad_korn@yahoo.com>
2022-02-14 06:16:13 -0800

    Client Implementation has been completed

--- src/main/java/com/mehrdad/sample/bank/view/UserInterface.java
+++ src/main/java/com/mehrdad/sample/bank/view/UserInterface.java
@@ -68,7 +68,6 @@ public class UserInterface {
             if (!foundClient.isActive()) {
                 activateOrDeactivateClient(foundClient);
             }
-
             return;
         }
 
