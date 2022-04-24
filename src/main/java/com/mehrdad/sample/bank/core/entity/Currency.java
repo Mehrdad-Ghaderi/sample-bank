069fe6b6a3bdb6ddd32fbe0541cf4cb6bd4e5e71
MehRdad <mehrdad_korn@yahoo.com>
2022-04-24 03:09:40 -0700

    Transaction implementation TransactionEntity is implemented

---
+++ src/main/java/com/mehrdad/sample/bank/core/entity/Currency.java
@@ -0,0 +1,12 @@
+package com.mehrdad.sample.bank.core.entity;
+
+public enum Currency {
+    //USD(1L ), CAD(2L), RIAL(3L), EURO(4L);
+    USD, CAD, EURO, RIAL
+
+    //private final    Long id;
+
+    /*Currency(Long id) {
+        this.id = id;
+    }*/
+}
