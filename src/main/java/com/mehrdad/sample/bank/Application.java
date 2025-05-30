ed9f95cc26cf4529e01089911e26adbd9711b3f7
Mehrdad Ghaderi <mehrdadghaderi83@gmail.com>
2025-05-29 20:22:06 -0400

    Add GitHub Actions CI for Spring Boot

--- src/main/java/com/mehrdad/sample/bank/Application.java
+++ src/main/java/com/mehrdad/sample/bank/Application.java
@@ -1,6 +1,8 @@
 package com.mehrdad.sample.bank;
 
 import com.mehrdad.sample.bank.view.UserInterface;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -10,6 +12,20 @@ import org.springframework.context.ApplicationContext;
  */
 @SpringBootApplication
 public class Application {
+
+    @Value("${spring.datasource.url}")
+    private String jdbcUrl;
+
+    @Value("${spring.datasource.username}")
+    private String dbUser;
+
+    @PostConstruct
+    public void logDbConfig() {
+        System.out.println("----------- DB CONFIG -----------");
+        System.out.println("URL: " + jdbcUrl);
+        System.out.println("USER: " + dbUser);
+        System.out.println("---------------------------------");
+    }
 
     public static void main(String[] args) {
         ApplicationContext applicationContext = SpringApplication.run(Application.class, args);
