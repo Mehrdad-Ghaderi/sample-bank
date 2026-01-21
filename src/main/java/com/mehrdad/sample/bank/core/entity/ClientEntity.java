c318423867601d894245e06e2ecfc7d76fdf6558
Mehrdad <mehrdadghaderi83@gmail.com>
2026-01-20 21:09:54 -0500

    -cleanup, final commit on status

--- src/main/java/com/mehrdad/sample/bank/core/entity/ClientEntity.java
+++ src/main/java/com/mehrdad/sample/bank/core/entity/ClientEntity.java
@@ -42,20 +42,9 @@ public class ClientEntity {
     @OneToMany(mappedBy = "client", fetch = FetchType.EAGER, cascade = CascadeType.ALL)
     private List<AccountEntity> accounts;
 
-//    @NotNull
-//    @Column(name = "active", nullable = false)
-//    private Boolean active;
-
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private Status status;
-
-
-    public ClientEntity(String id, String name, String phoneNumber) {
-        this.id = id;
-        this.name = name;
-        this.phoneNumber = phoneNumber;
-    }
 
     @Override
     public String toString() {
