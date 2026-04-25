e9f0180102c55dbf0ec92f9ccee815820367d753
Mehrdad <mehrdadghaderi83@gmail.com>
2026-04-24 23:37:30 -0400

    Reorder API path constants

--- src/main/java/com/mehrdad/sample/bank/api/ApiPaths.java
+++ src/main/java/com/mehrdad/sample/bank/api/ApiPaths.java
@@ -4,12 +4,12 @@ public final class ApiPaths {
 
     public static final String API_BASE_PATH = "/api/v1";
     public static final String CUSTOMERS = "/customers";
+    public static final String CUSTOMER_RESOURCE = API_BASE_PATH + CUSTOMERS + "/{customerId}";
     public static final String ACCOUNTS = "/accounts";
+    public static final String ACCOUNT_RESOURCE = API_BASE_PATH + ACCOUNTS + "/{accountId}";
     public static final String TRANSACTIONS = "/transactions";
     public static final String AUTH = "/auth";
     public static final String USERS = "/users";
-    public static final String CUSTOMER_RESOURCE = API_BASE_PATH + CUSTOMERS + "/{customerId}";
-    public static final String ACCOUNT_RESOURCE = API_BASE_PATH + ACCOUNTS + "/{accountId}";
     public static final String USER_RESOURCE = API_BASE_PATH + USERS + "/{userId}";
 
     private ApiPaths() {
